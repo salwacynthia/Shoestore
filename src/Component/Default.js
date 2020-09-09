715258195e3bea1d38c71f0a5d0d0d3258b29668
SalCy <46075019+salwacynthia@users.noreply.github.com>
2020-09-09 15:23:49 +0200

    Create Default.js

---
+++ src/Component/Default.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Default extends Component{
+    render(){
+        return(
+           <h2>page not found</h2> 
+        );
+    }
+}
+
+export default Default; 
