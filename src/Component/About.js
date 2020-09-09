7115f363b6cf5fd623b4a5ce191a7afff9490d33
SalCy <46075019+salwacynthia@users.noreply.github.com>
2020-09-09 15:22:26 +0200

    Create About.js

---
+++ src/Component/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+render () {
+    return (
+
+        <div className="container">
+
+        <div className="jumbotron" id = "about">
+          <h1 className="display-4">Shoe Passion!</h1>
+             <p className="lead">
+                 
+             Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park
+             actually green juice sriracha paleo. Brooklyn sriracha semiotics, 
+             DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf 
+             authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS,
+             </p>
+             <hr className="my-4"/>
+             <p>It uses utility classes for typography and spacing to space content out within the larger container.Vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical.
+            Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.
+            </p>
+            
+            
+        </div>
+        </div>
+  );
+}
+}
